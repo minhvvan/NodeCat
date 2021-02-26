6f4d9cfd24fc343e94095538e3d2d0951e2d4f69
GR8 <eminhwan12@gmail.com>
2021-02-26 15:11:30 +0900

    Use API

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,53 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const URL = 'http://localhost:8002/v1';
+
+axios.defaults.headers.origin = 'http://localhost:4000'; // origin 헤더 추가
+
+const request = async (req, api) => {
+    try{
+        if(!req.session.jwt) { //세션에 토큰이 없으면
+            const tokenResult = await axios.post(`${URL}/token`, {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+            req.session.jwt = tokenResult.data.token; //세션에 토큰 저장
+        }
+        return await axios.get(`${URL}${api}`,{
+            headers: { authorization: req.session.jwt},
+        }); //API 요청
+    }catch(err){
+        if(err.response.status === 419){ //토큰 만료시
+            delete req.session.jwt;
+            return request(req, api);
+        }//419 외의 다른 에러
+        return err.response;
+    }
+};
+
+router.get('/mypost', async(req, res, next) => {
+    try{
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
+    }catch(err){
+        console.error(err);
+        next(err);
+    }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+    try{
+        const result = await request(
+            req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+        );
+        res.json(result.data);
+    }catch(err){
+        if(err.code){
+            console.error(err);
+            next(err);
+        }
+    }
+});
 
 router.get('/test', async (req, res, next) => { //토큰 테스트 라우터
     try{
